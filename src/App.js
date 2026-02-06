20b1f6b194916be0290a74b91d9f74d3c2096097
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2026-02-06 16:53:56 +0200

    imaging

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
 const Header = styled.div``
-const Hero = styled.div``
+
+//------------------------------
+const Hero = styled.div`
+  width: 100%;
+  height: auto;
+`
+const Image = styled.img`
+ width: 100%;
+  height: auto;
+  display: block;
+`
+
+//-------------------------------
 const Main = styled.div``
+
+//--------------------------------
 const Graphic = styled.div``
+
+//-------------------------------
 const MusicPhoto = styled.div``
+
+//------------------------------
 const Slider = styled.div``
+
+//-----------------------------
 const Footer = styled.div``
+
+//-----------------------------
 const Modal = styled.div``
+
 class App extends Component {
   state = {}
   render () {
   return (
     <div className="App">
      <Header></Header>
-     <Hero></Hero>
+     <Hero>
+      <Image src="./photos/hero-header/byesun.webp"/>
+     </Hero>
      <Main></Main>
      <Graphic></Graphic>
      <MusicPhoto></MusicPhoto>
