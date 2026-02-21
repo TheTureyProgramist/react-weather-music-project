30adf052ff8296eb6081f4ad1d792bcbc40befb7
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2026-02-21 13:29:05 +0200

    public-start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -326,7 +326,7 @@ const Header = ({
   return (
     <HeaderDiv $isDarkMode={isDarkMode}>
       <HeaderFix>
-        <HeaderLogo src={logo} />
+        <HeaderLogo src={logo} alt="Логотип Стихії: Погода надійніша з нами!"/>
         {user && (
           <VipTextWrapper onClick={onOpenVip}>
             <RainbowText $show={!showUltra}>Стихія+</RainbowText>
