6a21eb803ccd8be853143a6e8cad0a7838e1dbcf
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2026-03-08 10:33:19 +0200

    starttransform

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Обов'язково для GitHub Pages
+  basePath: '/react-weather-music-project',
+  assetPrefix: '/react-weather-music-project',
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    unoptimized: true, // GitHub Pages не підтримує стандартну оптимізацію зображень Next.js
+  },
+};
+
+module.exports = nextConfig;
