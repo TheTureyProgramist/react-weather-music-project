79d91732706d3e0d8208efdee5f3572d2112bd24
TheTureyProgramist <150546608+TheTureyProgramist@users.noreply.github.com>
2026-03-15 07:43:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
+// import "modern-normalize/modern-normalize.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import "./index.css";
+// import App from "./App.js";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/react-weather-music-project"> 
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 import "modern-normalize/modern-normalize.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; 
 import "./index.css";
 import App from "./App.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <HashRouter> 
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
